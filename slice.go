b9531963f92e0d990d5812d85c08f00012a18430
hsldymq <hsldymq@qq.com>
2024-02-20 21:16:06 +0800

    feat: allow iter slice backward

--- slice.go
+++ slice.go
@@ -1,21 +1,31 @@
 package goiter
 
-import "iter"
+import (
+	"iter"
+)
 
-func SliceIter[T any](s []T) iter.Seq2[int, T] {
+func SliceIter[T any](s []T, backward ...bool) iter.Seq2[int, T] {
 	return func(yield func(int, T) bool) {
-		for idx, elem := range s {
-			if !yield(idx, elem) {
-				return
+		if len(backward) == 0 || !backward[0] {
+			for idx, elem := range s {
+				if !yield(idx, elem) {
+					return
+				}
+			}
+		} else {
+			for i := len(s) - 1; i >= 0; i-- {
+				if !yield(i, s[i]) {
+					return
+				}
 			}
 		}
 	}
 }
 
-func SliceIterElem[T any](s []T) iter.Seq[T] {
-	return PickV[int, T](SliceIter(s))
+func SliceIterElem[T any](s []T, backward ...bool) iter.Seq[T] {
+	return PickV[int, T](SliceIter(s, backward...))
 }
 
-func SliceIterIdx[T any](s []T) iter.Seq[int] {
-	return PickK[int, T](SliceIter(s))
+func SliceIterIdx[T any](s []T, backward ...bool) iter.Seq[int] {
+	return PickK[int, T](SliceIter(s, backward...))
 }
--- slice_test.go
+++ slice_test.go
@@ -7,14 +7,21 @@ import (
 )
 
 func TestSliceIter(t *testing.T) {
-	expect := []int{7, 8, 9}
-
-	actual := make([]int, 3)
-	for idx, v := range SliceIter(expect) {
-		actual[idx] = v
+	actual := make([]int, 0, 3)
+	for v := range SliceIterIdx([]int{7, 8, 9}) {
+		actual = append(actual, v)
 	}
-
+	expect := []int{0, 1, 2}
 	if !reflect.DeepEqual(expect, actual) {
 		t.Fatal(fmt.Sprintf("expect: %v, actual: %v", expect, actual))
+	}
+
+	actual2 := make([]int, 0, 3)
+	for idx := range SliceIterElem([]int{7, 8, 9}, true) {
+		actual2 = append(actual2, idx)
+	}
+	expect2 := []int{9, 8, 7}
+	if !reflect.DeepEqual(expect2, actual2) {
+		t.Fatal(fmt.Sprintf("expect: %v, actual: %v", expect2, actual2))
 	}
 }
