93a8919ca3c04d0acf4dd45502fb96a427366a9b
hsldymq <hsldymq@qq.com>
2024-02-24 23:34:29 +0800

    fix: internal int functions

--- int.go
+++ int.go
@@ -2,75 +2,25 @@
 
 package goiter
 
-import (
-	"iter"
-)
-
-func Int[T tInt, S sInt](start, stop T, _step ...S) iter.Seq[T] {
-	step := int64(1)
-	inc := true
-	if start > stop {
-		inc = false
-		step = -1
-	}
-
-	if len(_step) > 0 {
-		step = int64(_step[0])
-	}
-
-	emptyFunc := func(yield func(T) bool) {}
-	if step == 0 {
-		return emptyFunc
-	}
-	if (inc && step < 0) || (!inc && step > 0) {
-		return emptyFunc
-	}
-
-	return func(yield func(T) bool) {
-		curr := start
-		for {
-			if !yield(curr) {
-				return
-			}
-
-			if inc {
-				s := T(step)
-				if s > intMax(curr) {
-					return
-				}
-				next := curr + s
-				// TODO
-				_ = next
-			} else {
-
-			}
-		}
-	}
-}
-
 type tInt interface {
-	~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | sInt
-}
-
-type sInt interface {
-	~int | ~int8 | ~int16 | ~int32 | ~int64
+	~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 |
+		~int | ~int8 | ~int16 | ~int32 | ~int64
 }
 
 func intMin[T tInt](v T) T {
-	allOnes := ^T(0)
-	if allOnes < 0 {
-		return allOnes
+	ones := ^T(0)
+	if ones < 0 {
+		return ^(ones ^ (1 << (countBits(ones) - 1)))
 	}
 	return 0
 }
 
 func intMax[T tInt](v T) T {
-	allOnes := ^T(0)
-	if allOnes < 0 {
-		return allOnes ^ (1 << (countBits(allOnes) - 1))
+	ones := ^T(0)
+	if ones < 0 {
+		return ones ^ (1 << (countBits(ones) - 1))
 	}
-
-	return allOnes
+	return ones
 }
 
 func countBits[T tInt](v T) int {
@@ -80,6 +30,5 @@ func countBits[T tInt](v T) int {
 			return bits
 		}
 	}
-
 	return 64
 }
