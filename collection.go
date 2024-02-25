8b26b64c8a1f31870a5424cbae729fa1a289aa23
hsldymq <hsldymq@qq.com>
2024-02-25 16:53:56 +0800

    update: comments

--- collection.go
+++ collection.go
@@ -6,6 +6,7 @@ import (
 	"iter"
 )
 
+// Slice returns an iterator that allows you to traverse a slice in a forward or reverse direction.
 func Slice[T any](s []T, backward ...bool) iter.Seq2[int, T] {
 	return func(yield func(int, T) bool) {
 		if len(backward) == 0 || !backward[0] {
@@ -24,14 +25,17 @@ func Slice[T any](s []T, backward ...bool) iter.Seq2[int, T] {
 	}
 }
 
+// SliceElem only yields the elements of a slice.
 func SliceElem[T any](s []T, backward ...bool) iter.Seq[T] {
 	return PickV[int, T](Slice(s, backward...))
 }
 
+// SliceIdx only yields the indices of a slice.
 func SliceIdx[T any](s []T, backward ...bool) iter.Seq[int] {
 	return PickK[int, T](Slice(s, backward...))
 }
 
+// Map returns an iterator that allows you to traverse a map.
 func Map[K comparable, V any](m map[K]V) iter.Seq2[K, V] {
 	return func(yield func(K, V) bool) {
 		for k, v := range m {
@@ -42,14 +46,17 @@ func Map[K comparable, V any](m map[K]V) iter.Seq2[K, V] {
 	}
 }
 
+// MapVal only yields the values of a map.
 func MapVal[K comparable, V any](m map[K]V) iter.Seq[V] {
 	return PickV[K, V](Map(m))
 }
 
+// MapKey only yields the keys of a map.
 func MapKey[K comparable, V any](m map[K]V) iter.Seq[K] {
 	return PickK[K, V](Map(m))
 }
 
+// Channel yields the values from a channel, it will stop when the channel is closed.
 func Channel[T any](c <-chan T) iter.Seq[T] {
 	return func(yield func(T) bool) {
 		for v := range c {
@@ -60,6 +67,7 @@ func Channel[T any](c <-chan T) iter.Seq[T] {
 	}
 }
 
+// Filter returns an iterator that only yields the values of the input iterator that satisfy a predicate.
 func Filter[T any](seq iter.Seq[T], predicate func(T) bool) iter.Seq[T] {
 	return func(yield func(T) bool) {
 		next, stop := iter.Pull(seq)
@@ -79,6 +87,7 @@ func Filter[T any](seq iter.Seq[T], predicate func(T) bool) iter.Seq[T] {
 	}
 }
 
+// Filter2 returns an iterator that only yields the key-values of the input iterator that satisfy a predicate.
 func Filter2[K any, V any](seq iter.Seq2[K, V], predicate func(K, V) bool) iter.Seq2[K, V] {
 	return func(yield func(K, V) bool) {
 		next, stop := iter.Pull2(seq)
--- transform.go
+++ transform.go
@@ -4,24 +4,29 @@ package goiter
 
 import "iter"
 
+// PickK yields the keys of a sequence of key-value pairs.
 func PickK[K, V any](seq iter.Seq2[K, V]) iter.Seq[K] {
 	return T21(seq, func(k K, _ V) K {
 		return k
 	})
 }
 
+// PickV yields the values of a sequence of key-value pairs.
 func PickV[K, V any](seq iter.Seq2[K, V]) iter.Seq[V] {
 	return T21(seq, func(_ K, v V) V {
 		return v
 	})
 }
 
+// SwapKV yields key-value pairs after swapping the position of the keys and values obtained by the input iterator.
 func SwapKV[K, V any](seq iter.Seq2[K, V]) iter.Seq2[V, K] {
 	return T2(seq, func(k K, v V) (V, K) {
 		return v, k
 	})
 }
 
+// T1 return a transforming iterator, where T stands for transform.
+// It applies the transformer function to the values obtained from the input iterator, and then yields the result.
 func T1[In, Out any](
 	seq iter.Seq[In],
 	transformer func(In) Out,
@@ -41,6 +46,7 @@ func T1[In, Out any](
 	}
 }
 
+// T2 is similar to T1, but it obtains the key-value pairs from the input iterator and yields new key-value pairs after transformation.
 func T2[InK, InV, OutK, OutV any](
 	seq iter.Seq2[InK, InV],
 	transformer func(InK, InV) (OutK, OutV),
@@ -60,6 +66,7 @@ func T2[InK, InV, OutK, OutV any](
 	}
 }
 
+// T12 is similar to T1, but it obtains the values from the input iterator and yields new key-value pairs after transformation.
 func T12[In, OutK, OutV any](
 	seq iter.Seq[In],
 	transformer func(In) (OutK, OutV),
@@ -79,6 +86,7 @@ func T12[In, OutK, OutV any](
 	}
 }
 
+// T21 is similar to T1, but it obtains the values from the input iterator and yields new key-value pairs after transformation.
 func T21[InK, InV, Out any](
 	seq iter.Seq2[InK, InV],
 	transformer func(InK, InV) Out,
