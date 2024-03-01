4fe3416e756760237672d90da8b1b6a1ab5ffd8c
hsldymq <hsldymq@qq.com>
2024-03-01 16:52:07 +0800

    update: tests

--- range_test.go
+++ range_test.go
@@ -157,6 +157,22 @@ func TestRangeTime(t *testing.T) {
 		t.Fatalf("test RangeTime failed, expect %v, got %v", expect, actual)
 	}
 
+	from, _ = time.Parse(time.DateTime, "2024-01-01 00:00:00")
+	to, _ = time.Parse(time.DateTime, "2024-01-05 00:00:00")
+	actual = make([]string, 0)
+	i := 0
+	for t := range RangeTime(from, to, 24*time.Hour) {
+		actual = append(actual, t.Format(time.DateOnly))
+		i++
+		if i >= 3 {
+			break
+		}
+	}
+	expect = []string{"2024-01-01", "2024-01-02", "2024-01-03"}
+	if !slices.Equal(expect, actual) {
+		t.Fatalf("test RangeTime failed, expect %v, got %v", expect, actual)
+	}
+
 	from, _ = time.Parse(time.DateTime, "2024-01-01 03:10:00")
 	to, _ = time.Parse(time.DateTime, "2024-01-01 00:00:00")
 	actual = make([]string, 0)
@@ -164,6 +180,22 @@ func TestRangeTime(t *testing.T) {
 		actual = append(actual, t.Format(time.DateTime))
 	}
 	expect = []string{"2024-01-01 03:10:00", "2024-01-01 02:10:00", "2024-01-01 01:10:00", "2024-01-01 00:10:00"}
+	if !slices.Equal(expect, actual) {
+		t.Fatalf("test RangeTime failed, expect %v, got %v", expect, actual)
+	}
+
+	from, _ = time.Parse(time.DateTime, "2024-01-01 03:10:00")
+	to, _ = time.Parse(time.DateTime, "2024-01-01 00:00:00")
+	actual = make([]string, 0)
+	i = 0
+	for t := range RangeTime(from, to, time.Hour) {
+		actual = append(actual, t.Format(time.DateTime))
+		i++
+		if i >= 3 {
+			break
+		}
+	}
+	expect = []string{"2024-01-01 03:10:00", "2024-01-01 02:10:00", "2024-01-01 01:10:00"}
 	if !slices.Equal(expect, actual) {
 		t.Fatalf("test RangeTime failed, expect %v, got %v", expect, actual)
 	}
