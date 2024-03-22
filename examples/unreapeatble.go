d5894f6dd387429dcd58a5b3e2214217030ccf23
hsldymq <hsldymq@qq.com>
2024-03-22 20:07:48 +0800

    update: example comments

--- examples/unreapeatble.go
+++ examples/unreapeatble.go
@@ -8,11 +8,11 @@ import (
 func OnceDemo() {
 	data := []int{1, 2, 3, 4, 5, 6}
 
-	// this is an example for Once function
-	// iterator returned by Once can be only used once
+	// this is an example for Once function.
+	// iterator returned by Once can be only used once.
 	iterator := goiter.Once(goiter.SliceElem(data))
 
-	// break the loop or finish the iteration will make the iterator unusable
+	// break the loop or finish the iteration will make the iterator unusable.
 	for v := range iterator {
 		// only the first 3 elements will be printed: 1 2 3
 		fmt.Printf("%d ", v)
@@ -30,10 +30,10 @@ func OnceDemo() {
 func ContinuableOnceDemo() {
 	data := []int{1, 2, 3, 4, 5, 6}
 
-	// this is an example for ContinuableOnce function
+	// this is an example for ContinuableOnce function.
 	iterator := goiter.ContinuableOnce(goiter.SliceElem(data))
 
-	// break the loop midway will not make the iterator unusable until all elements have been yielded
+	// break the loop midway will not make the iterator unusable until all elements have been yielded.
 	for v := range iterator {
 		// only the first 3 elements will be printed: 1 2 3
 		fmt.Printf("%d ", v)
