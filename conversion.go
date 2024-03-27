1d59d0f211a145afdbcde261a346bb411c2a9eba
hsldymq <hsldymq@qq.com>
2024-03-27 18:47:55 +0800

    allow conversion functions to accept more general iterator types

--- conversion.go
+++ conversion.go
@@ -3,30 +3,30 @@
 package goiter
 
 // ToSlice converts an iterator to a slice.
-func ToSlice[T any](it Iterator[T]) []T {
+func ToSlice[TIter SeqX[T], T any](iterator TIter) []T {
 	var result []T
-	for each := range it {
+	for each := range iterator {
 		result = append(result, each)
 	}
 	return result
 }
 
 // ToMap converts an iterator that yields 2-tuple to a map, where the first element of the tuple is the key and the second element is the value.
-func ToMap[T1 comparable, T2 any](it Iterator2[T1, T2]) map[T1]T2 {
+func ToMap[TIter Seq2X[T1, T2], T1 comparable, T2 any](iterator TIter) map[T1]T2 {
 	result := make(map[T1]T2)
-	for key, val := range it {
+	for key, val := range iterator {
 		result[key] = val
 	}
 	return result
 }
 
 // ToMapAs transform every element provided from the input iterator to a key-value pair, and then returns a map.
-func ToMapAs[T any, OutK comparable, OutV any](
-	it Iterator[T],
-	transformer func(T) (OutK, OutV),
-) map[OutK]OutV {
-	result := make(map[OutK]OutV)
-	for v := range it {
+func ToMapAs[TIter SeqX[T], TK comparable, TV any, T any](
+	iterator TIter,
+	transformer func(T) (TK, TV),
+) map[TK]TV {
+	result := make(map[TK]TV)
+	for v := range iterator {
 		key, val := transformer(v)
 		result[key] = val
 	}
@@ -34,12 +34,12 @@ func ToMapAs[T any, OutK comparable, OutV any](
 }
 
 // ToMap2As is similar to ToMapAs, but it takes 2-Tuple from the input iterator.
-func ToMap2As[InT1 any, InT2 any, OutK comparable, OutV any](
-	it Iterator2[InT1, InT2],
-	transformer func(InT1, InT2) (OutK, OutV),
-) map[OutK]OutV {
-	result := make(map[OutK]OutV)
-	for v1, v2 := range it {
+func ToMap2As[TIter Seq2X[T1, T2], TK comparable, TV any, T1 any, T2 any](
+	iterator TIter,
+	transformer func(T1, T2) (TK, TV),
+) map[TK]TV {
+	result := make(map[TK]TV)
+	for v1, v2 := range iterator {
 		key, val := transformer(v1, v2)
 		result[key] = val
 	}
