410a3995593b873c0967294f5dd3823cfa41aee2
hsldymq <hsldymq@qq.com>
2024-06-27 00:50:39 +0800

    update comment

--- unrepeatable.go
+++ unrepeatable.go
@@ -1,5 +1,3 @@
-//go:build goexperiment.rangefunc
-
 package goiter
 
 import (
@@ -9,8 +7,8 @@ import (
 )
 
 // Once returns an iterator that can only be iterated over once;
-// it cannot be reused after the iteration is complete or after breaking out of the loop. On subsequent attempts, it will not yield any values.
-// you can not also iterate over it concurrently, only one goroutine can iterate over it.
+// It cannot be reused after the iteration is complete or after breaking out of the loop. On subsequent attempts, it will yield nothing.
+// Similarly, you cannot iterate over it in multiple goroutines. If you do so, only one goroutine will produce values.
 func Once[TIter SeqX[T], T any](iterator TIter) Iterator[T] {
     flag := int32(0)
     return func(yield func(T) bool) {
@@ -33,7 +31,7 @@ func Once[TIter SeqX[T], T any](iterator TIter) Iterator[T] {
     }
 }
 
-// Once2 is the Iterator2 version of Once.
+// Once2 is the iter.Seq2 version of Once function.
 func Once2[TIter Seq2X[T1, T2], T1, T2 any](iterator TIter) Iterator2[T1, T2] {
     flag := int32(0)
     return func(yield func(T1, T2) bool) {
@@ -56,9 +54,9 @@ func Once2[TIter Seq2X[T1, T2], T1, T2 any](iterator TIter) Iterator2[T1, T2] {
     }
 }
 
-// FinishOnce unlike Once, FinishOnce can be iterated over multiple times until all values have been yields exactly once.
-// so you can break out of the iteration midway, and then iterate continuously from where you left off.
-// you can also iterate over it concurrently, FinishOnce will make sure that all values are yielded exactly once.
+// FinishOnce unlike Once function, it can be iterated over multiple times until all values have been yielded exactly once.
+// This means you can break out of the iteration midway and then continue iterating from where you left off.
+// You can also iterate over it concurrently; FinishOnce will ensure that all values are yielded exactly once.
 // once all values have been yielded, it will not yield any more values.
 func FinishOnce[TIter SeqX[T], T any](iterator TIter) Iterator[T] {
     fetchLock := &sync.Mutex{}
@@ -86,7 +84,7 @@ func FinishOnce[TIter SeqX[T], T any](iterator TIter) Iterator[T] {
     }
 }
 
-// FinishOnce2 is the Iterator2 version of FinishOnce.
+// FinishOnce2 is the iter.Seq2 version of FinishOnce function.
 func FinishOnce2[TIter Seq2X[T1, T2], T1, T2 any](iterator TIter) Iterator2[T1, T2] {
     fetchLock := &sync.Mutex{}
     next, stop := iter.Pull2(iter.Seq2[T1, T2](Once2(iterator)))
